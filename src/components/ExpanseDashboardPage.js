c8ee6b65764f125256932358091fcdad637e7114
Taye <tayejoseph6@gmail.com>
2018-03-15 13:00:20 +0100

    Add expense summary to dashboard

--- src/components/ExpanseDashboardPage.js
+++ src/components/ExpanseDashboardPage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from "./ExpenseListFilters";
- 
+import ExpensesSummary from "./ExpensesSummary";
 
 //I think we didn't export ExpanseDashboardPage directly because it does not contains props
 //that we want to also test unlike the ExpenseList in the Expenselist page
 const ExpanseDashboardPage = () => (
     <div>
+    <ExpensesSummary />
     <ExpenseListFilters />
     <ExpenseList />    
     </div>
